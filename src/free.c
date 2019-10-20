0e1476b1108a19d5eeba2c08f43ea960895db8f0
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-10-20 11:33:23 +0200

    fix free

--- src/free.c
+++ src/free.c
@@ -14,73 +14,78 @@
 
 inline static int		should_munmap(t_alloc *cur)
 {
-    t_alloc		*next;
-    t_alloc		*prev;
+	t_alloc		*next;
+	t_alloc		*prev;
 
-    next = cur->next;
-    prev = cur->prev;
-    return ((!prev && !next) || (!prev && next && next->zone != cur->zone) ||
-            (!next && prev && prev->zone != cur->zone) ||
-            (next && prev && prev->zone != cur->zone && next->zone != cur->zone));
+	next = cur->next;
+	prev = cur->prev;
+	return ((!prev && !next) || (!prev && next && next->zone != cur->zone) ||
+			(!next && prev && prev->zone != cur->zone) ||
+			(next && prev && prev->zone != cur->zone && next->zone != cur->zone));
 }
 
 inline static void		defrag(t_alloc **e)
 {
-    t_alloc		*next;
-    t_alloc		*prev;
+	t_alloc		*next;
+	t_alloc		*prev;
 
-    prev = (*e)->prev;
-    next = (*e)->next;
-    while (prev && prev->free && prev->zone == (*e)->zone)
-    {
-        prev->size += HEADER + (*e)->size;
-        prev->next = (*e)->next;
-        *e = prev;
-        if (next)
-            next->prev = *e;
-        prev = prev->prev;
-    }
-    while (next && next->free && next->zone == (*e)->zone)
-    {
-        (*e)->size += HEADER + next->size;
-        next = next->next;
-        (*e)->next = next;
-        if (next)
-            next->prev = *e;
-    }
+	prev = (*e)->prev;
+	next = (*e)->next;
+	while (prev && prev->free && prev->zone == (*e)->zone)
+	{
+		prev->size += HEADER + (*e)->size;
+		prev->next = (*e)->next;
+		*e = prev;
+		if (next)
+			next->prev = *e;
+		prev = prev->prev;
+	}
+	while (next && next->free && next->zone == (*e)->zone)
+	{
+		(*e)->size += HEADER + next->size;
+		next = next->next;
+		(*e)->next = next;
+		if (next)
+			next->prev = *e;
+	}
+}
+
+inline static void		remove_zone(t_alloc **zone, t_alloc *cur)
+{
+	t_alloc *prev;
+	t_alloc *next;
+	prev = cur->prev;
+	next = cur->next;
+	if (!prev)
+	{
+		if ((*zone = next) != NULL)
+			(*zone)->prev = NULL;
+	}
+	else
+	{
+		prev->next = next;
+		if (next)
+			next->prev = prev;
+	}
+	munmap(cur, cur->size + HEADER);
 }
 
 void					free_unthread(void *ptr)
 {
-    t_alloc		*cur;
-    t_alloc     *prev;
-    t_alloc     *next;
-    t_alloc		**zone;
+	t_alloc		*cur;
+	t_alloc		**zone;
 
-    if (!ptr || (zone = get_zone_set_cur(ptr, &cur)) == NULL)
-        return ;
-    cur->free = 1;
-    defrag(&cur);
-    if (should_munmap(cur))
-    {
-        prev = cur->prev;
-        next = cur->next;
-        if (!prev && (*zone = next) != NULL)
-            (*zone)->prev = NULL;
-        else
-        {
-            prev->next = next;
-            if (next)
-                next->prev = prev;
-        }
-        if (munmap(cur, cur->size + HEADER) == -1)
-            return ;
-    }
+	if (!ptr || (zone = get_zone_set_cur(ptr, &cur)) == NULL)
+		return ;
+	cur->free = 1;
+	defrag(&cur);
+	if (should_munmap(cur))
+		remove_zone(zone, cur);
 }
 
 void					free(void *ptr)
 {
-    pthread_mutex_lock(&g_mutex);
-    free_unthread(ptr);
-    pthread_mutex_unlock(&g_mutex);
+	pthread_mutex_lock(&g_mutex);
+	free_unthread(ptr);
+	pthread_mutex_unlock(&g_mutex);
 }
--- src/malloc.c
+++ src/malloc.c
@@ -14,118 +14,114 @@
 
 inline static void	*new_zone(int zone_index, size_t size, size_t zone_size)
 {
-    size_t		mmap_size;
-    size_t		next_size;
-    t_alloc		*new;
-    t_alloc		*ptr;
+	size_t		mmap_size;
+	t_alloc		*new;
+	t_alloc		*ptr;
 
-    mmap_size = get_multiple_of(zone_size, PS);
-    ptr = (t_alloc *)mmap(0, mmap_size, PROT, FLAGS, -1, 0);
-    if (ptr == MAP_FAILED)
-        return (NULL);
+	mmap_size = get_multiple_of(zone_size, PS);
+	ptr = (t_alloc *)mmap(0, mmap_size, PROT, FLAGS, -1, 0);
+	if (ptr == MAP_FAILED)
+		return (NULL);
 
-    ptr->size = size;
-    ptr->free = 0;
-    ptr->zone = zone_index;
-    ptr->next = NULL;
+	ptr->size = size;
+	ptr->free = 0;
+	ptr->zone = zone_index;
+	ptr->next = NULL;
 
-    if (!(next_size = mmap_size - (HEADER * 2 + size)))
-        return (ptr);
+	new = (void *)ptr + HEADER + size;
+	new->size = mmap_size - (HEADER * 2 + size);
+	new->zone = zone_index;
+	new->free = 1;
+	new->next = NULL;
+	new->prev = ptr;
 
-    new = (void *)ptr + HEADER + size;
-    new->size = next_size;
-    new->zone = zone_index;
-    new->free = 1;
-    new->next = NULL;
-    new->prev = ptr;
+	ptr->next = new;
 
-    ptr->next = new;
-
-    return (ptr);
+	return (ptr);
 }
 
 inline static void	create_free_block(t_alloc **cur, size_t size)
 {
-    t_alloc     *new;
+	t_alloc     *new;
 
-    new = (void *)*cur + HEADER + size;
-    new->free = 1;
-    new->size = (*cur)->size - (HEADER + size);
-    new->zone = (*cur)->zone;
-    new->next = (*cur)->next;
-    new->prev = *cur;
+	new = (void *)*cur + HEADER + size;
+	new->free = 1;
+	new->size = (*cur)->size - (HEADER + size);
+	new->zone = (*cur)->zone;
+	new->next = (*cur)->next;
+	new->prev = *cur;
 
-    (*cur)->next = new;
-    (*cur)->size = size;
+	(*cur)->next = new;
+	(*cur)->size = size;
 }
 
 inline static void	*allocate(t_alloc **ptr, size_t size, size_t zone_size)
 {
-    t_alloc		*alloc;
-    t_alloc		*new;
+	t_alloc		*alloc;
+	t_alloc		*new;
 
-    if (!*ptr)
-    {
-        if (!(*ptr = new_zone(0, size, zone_size)))
-            return (NULL);
-        (*ptr)->prev = NULL;
-        return ((void *)*ptr + HEADER);
-    }
+	if (!*ptr)
+	{
+		if (!(*ptr = new_zone(0, size, zone_size)))
+			return (NULL);
+		(*ptr)->prev = NULL;
+		return ((void *)*ptr + HEADER);
+	}
 
-    alloc = *ptr;
+	alloc = *ptr;
 
-    while (alloc->next && (!alloc->free || alloc->size < size))
-        alloc = alloc->next;
+	while (alloc->next && (!alloc->free || alloc->size < size))
+		alloc = alloc->next;
 
-    if (!alloc->free || alloc->size < size)
-    {
-        if (!(new = new_zone(alloc->zone + 1, size, zone_size)))
-            return (NULL);
-        alloc->next = new;
-        new->prev = alloc;
-        return ((void *)new + HEADER);
-    }
+	if (!alloc->free || alloc->size < size)
+	{
+		if (!(new = new_zone(alloc->zone + 1, size, zone_size)))
+			return (NULL);
+		alloc->next = new;
+		new->prev = alloc;
+		return ((void *)new + HEADER);
+	}
 
-    alloc->free = 0;
+	alloc->free = 0;
 
-    if (alloc->size < size + HEADER + 16)
-        return ((void *)alloc + HEADER);
-    create_free_block(&alloc, size);
+	if (alloc->size < size + HEADER + 16)
+		return ((void *)alloc + HEADER);
+	create_free_block(&alloc, size);
 
-    return ((void *)alloc + HEADER);
+	return ((void *)alloc + HEADER);
 }
 
 void				*malloc_unthread(size_t size)
 {
-    size_t	aligned_size;
+	size_t	aligned_size;
 
-    aligned_size = get_multiple_of(size, 16);
-    if (aligned_size <= TINY)
-        return (allocate(&g_state.tiny, aligned_size, TINY_ZONE));
-    else if (aligned_size <= SMALL)
-        return (allocate(&g_state.small, aligned_size, SMALL_ZONE));
-    else
-        return (allocate(&g_state.large, aligned_size, aligned_size + HEADER));
+	aligned_size = get_multiple_of(size, 16);
+	if (aligned_size <= TINY)
+		return (allocate(&g_state.tiny, aligned_size, TINY_ZONE));
+	else if (aligned_size <= SMALL)
+		return (allocate(&g_state.small, aligned_size, SMALL_ZONE));
+	else
+		return (allocate(&g_state.large, aligned_size, aligned_size + HEADER));
 }
 
 void				*malloc(size_t size)
 {
-    static int	initialization = 1;
-    void		*ret;
+	static int	initialization = 1;
+	void		*ret;
 
-    if (initialization)
-        pthread_mutex_init(&g_mutex, NULL);
-    pthread_mutex_lock(&g_mutex);
+	if (initialization)
+		pthread_mutex_init(&g_mutex, NULL);
+	pthread_mutex_lock(&g_mutex);
 
-    if (initialization)
-    {
-        g_state.tiny = NULL;
-        g_state.small = NULL;
-        g_state.large = NULL;
-        initialization = 0;
-    }
+	if (initialization)
+	{
+		g_state.tiny = NULL;
+		g_state.small = NULL;
+		g_state.large = NULL;
+		initialization = 0;
+	}
 
-    ret = malloc_unthread(size);
-    pthread_mutex_unlock(&g_mutex);
-    return (ret);
+	ret = malloc_unthread(size);
+	pthread_mutex_unlock(&g_mutex);
+	return (ret);
 }
