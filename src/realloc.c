35ca1a9ae5b00d6911e62137ebc5c4aeca285cd3
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-10-24 17:20:25 +0200

    realloc remove resizing because its a pain

--- src/realloc.c
+++ src/realloc.c
@@ -12,73 +12,25 @@
 
 #include "ft_malloc.h"
 
-static int		should_resize(t_alloc *cur, size_t size)
-{
-	t_alloc		*next;
-
-	next = cur->next;
-	return (next
-			&& next->free
-			&& next->zone == cur->zone
-			&& size < cur->size + HEADER + next->size);
-}
-
-static void	resize_alloc(t_alloc *cur, size_t size)
-{
-	t_alloc		*next;
-	t_alloc     *new;
-	size_t		new_size;
-
-	next = cur->next;
-	new_size = next->size - (size - cur->size);
-	if (new_size < 16)
-	{
-		cur->size += HEADER + next->size;
-		next = next->next;
-		cur->next = next;
-		if (next)
-			next->prev = cur;
-	}
-	else
-	{
-		next = next->next;
-		cur->size = size;
-		new = (void *)cur + HEADER + size;
-		cur->next = new;
-		new->prev = cur;
-		new->next = next;
-		new->size = new_size;
-		new->zone = cur->zone;
-		new->free = 1;
-		if (next)
-			next->prev = new;
-	}
-}
-
 static void			*reall_unthread(void *ptr, size_t size, int should_free)
 {
-	t_alloc     *cur;
+	t_alloc     *block;
 	void        *new_ptr;
 	t_alloc		**zone;
 
-	cur = NULL;
-	if (!(zone = get_zone_set_cur(ptr, &cur)))
+	block = NULL;
+	if (!(zone = get_zone_set_cur(ptr, &block)))
 		return (NULL);
 	size = get_multiple_of(size, 16);
-	if (size <= cur->size)
+	if (size <= block->size)
 		return (ptr);
-	if (should_resize(cur, size))
-	{
-		resize_alloc(cur, size);
-		return (ptr);
-	}
 	if (!(new_ptr = malloc_unthread(size)))
 	{
 		if (should_free)
 			free_unthread(ptr);
 		return (NULL);
 	}
-	copy_memory(new_ptr, ptr, cur->size);
+	copy_memory(new_ptr, ptr, block->size);
 	free_unthread(ptr);
 	return (new_ptr);
 }
